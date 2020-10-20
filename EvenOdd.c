c63a243f60fa4d3bf9d843998a01c1ce0cad3a27
Naeem kathawala <Naeemkathawala@gmail.com>
2020-10-20 13:40:45 +0530

    same logic with python code
    for less code and batter result

--- EvenOdd.c
+++ EvenOdd.c
@@ -15,3 +15,14 @@ int main()
    }
    return 0;
 }
+
+# rogram to check whether a given number is even or odd python version 
+
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+   print("{0} is Even".format(num))
+else:
+   print("{0} is Odd".format(num))
+      
+      
+   #python code use less code with batter fast result .
